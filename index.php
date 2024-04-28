c3db558631f34e63e80f88abf3b356ec3e5f8b32
Sabah Raad <raad@aamarpay.net>
2024-04-28 15:38:00 +0600

    ci/cd check

--- index.php
+++ index.php
@@ -160,6 +160,7 @@ html, body {
                     <div class="form-items">
                         <h3>See Sample Gateway</h3>
                         <p>Fill in the data below.</p>
+                        <p>Fill in the data below.</p>
                         <form class="requires-validation" action="process.php" method="post">
                             <div class="col-md-12">
                             <input type="text" name="full_name" placeholder="Enter your full name">
